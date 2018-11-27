74c3f4b4f2f9b927d0688f2b1940d6e188f82e7c
kovner chalumo <kchalumo@gmail.com>
2018-11-27 00:33:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  prime = false
+  sqr = (number.to_f ** .5).to_i 
+  for i in 2..sqr do |divisor|
+    if number % divisor == 0 
+      prime = true 
+      break 
+    end
+  end
+  return prime 
+end
