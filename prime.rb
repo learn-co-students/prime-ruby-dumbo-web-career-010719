7a9b2bc0d6b586d46d2bd93a4403c1c7837ff6dd
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-12 18:06:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  num2 = num - 1
+  while num2 > 1
+    if (num % num2 == 0)
+      return false
+    end
+    num2 -= 1
+  end
+  return true
+end
+#sdgasdfa
