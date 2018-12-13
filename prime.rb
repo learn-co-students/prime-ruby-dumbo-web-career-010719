680e4ca1f5fe0ab7e2889f53b4518341af518b80
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-13 23:21:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  number = number.abs
+  if number == 2 or number == 3
+    return true
+  elsif number == 1 or number == 0
+    return false
+  end
+  
+  (2...number).each do |n|
+    if number % n == 0
+      return false
+    end
+  end
+  
+  return true
+end
