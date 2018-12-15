35369f1093f9246fc4a758fd3a1c1d65bc4f42c2
Simon Kong <simon.kong95@gmail.com>
2018-12-15 00:20:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
 # Add  code here!
+def prime?(n)
+  i=1
+  j=0
+  if n == 1
+    return false
+  elsif n == 2
+    return true
+  elsif n == 3
+    return true
+  elsif n < 0
+    return false
+  elsif n%2 == 0
+    return false
+  end
+
+  while i < n
+    if n%i == 0
+      j+=1
+    end
+    i+=1
+  end
+
+  if j > 2
+    return false
+  else
+    return true
+  end
+end
