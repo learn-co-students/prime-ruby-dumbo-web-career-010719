0b08529600501ac05dec420ab77957514ac0583b
zaheer <m.abbaszaheer@gmail.com>
2018-12-21 21:53:07 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,26 +1,21 @@
 #Method 1
 #The none? method takes an enumerable collection and tests whether the given condition 
 #is true for exactly NONE of the items in the collection
-
+=begin
  def prime?(number)
       return false if number < 2
       (2..Math.sqrt(number)).none? {|item|  number%item == 0}
  end
 
+=end
+
 # Method 2
-=begin
 def prime?(integer)
 	
-	return false if integer<2 
-
-  integers = (2..Math.sqrt(integer))
+	  return false if integer<2  
     @is_prime=true # suppose that it is prime number 
-    integers.each do |num|
-       	   if integer%num==0
-       	   @is_prime=false # it is proved that if it is divisible by any number, it is not prime.
-       	   break
-       	   end
+    (2.. integer ** 0.5).each do |num|
+       	 return false  if integer%num==0 #since it is divisible by num, it is not prime.
    end
   @is_prime
 end
-=end
