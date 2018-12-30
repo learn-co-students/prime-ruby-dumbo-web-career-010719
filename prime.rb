243f213b7a3cc82c38dc1c190f564ea0fd4dc02b
github username <github email address>
2018-12-30 08:01:42 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+
+def prime?(n)
+
+  # accounts for negative numbers
+  if n <=1
+  false
+
+  # accounts for 2 being prime
+  elsif n==2
+    true
+
+  # checks divisors...accounts for prime numbers
+  elsif  (2..n/2).none? {|i| n % i == 0}
+    true
+
+  # accounts for even numbers
+  elsif  n/2
+    false
+
+  end
+end
